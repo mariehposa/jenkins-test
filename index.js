ce9fae4fb628584757c465ee70ee2fd30c2b514d
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2021-03-15 08:40:09 +0100

    update server

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
   res.status(200).json("Hello world!");
 });
 
-module.exports = app.listen(4000, () =>
-  console.log(`Running on http://localhost:4000`)
+app.listen(process.env.PORT || 4000, () =>
+  console.log("Running on http://localhost:4000")
 );
